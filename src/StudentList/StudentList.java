f6846871244eb1d67862abe16c0151d2572273c0
pate3536 <pate3536@sheridancollege.ca>
2022-01-19 15:04:36 -0500

    Array size changed from 3 to 4

--- src/StudentList/StudentList.java
+++ src/StudentList/StudentList.java
@@ -7,7 +7,7 @@ package StudentList;
 import studentlist.Student;
 public class StudentList {
     public static void main(String[] args){
-        Student[] studentList = new Student[3];
+        Student[] studentList = new Student[4];
         studentList[0] = new Student("1","Ronak Sheth");
         studentList[1] = new Student("2","John Doe");
         studentList[2] = new Student("3","abc");
